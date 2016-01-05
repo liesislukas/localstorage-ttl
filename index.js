6b4b7fda5312712c518fee158f9888dd8ed8b559
Lukas Liesis <liesislukas@gmail.com>
2016-01-05 22:00:16 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var ls = {
+    set: function (variable, value, ttl_ms) {
+        var data = {value: value, expires_at: new Date().getTime() + ttl_ms / 1};
+        localStorage.setItem(variable.toString(), JSON.stringify(data));
+    },
+    get: function (variable) {
+        var data = JSON.parse(localStorage.getItem(variable.toString()));
+        if (data !== null) {
+            if (data.expires_at !== null && data.expires_at < new Date().getTime()) {
+                localStorage.removeItem(variable.toString());
+            } else {
+                return data.value;
+            }
+        }
+        return null;
+    }
+};
+
+module.exports = ls;
