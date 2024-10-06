33f800ce1bef93f40b5d40f4fa01950ebe62e9a6
Lukas Liesis <>
2024-10-06 19:20:06 +0300

    new version, after 9yr of JS published new TS version for the next...decade :)

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,37 @@
 function set(variable, value, ttl_ms) {
+  if (localStorage) {
     let data = {
-        value: value,
+      value: value,
     };
     if (ttl_ms) {
-        data.expires_at = Date.now() + ttl_ms;
+      data.expires_at = Date.now() + ttl_ms;
     }
     localStorage.setItem(variable, JSON.stringify(data));
+  }
 }
 function remove(variable) {
+  if (localStorage) {
     localStorage.removeItem(variable);
+  }
 }
 function get(variable) {
+  if (localStorage) {
     const item = localStorage.getItem(variable);
     if (item) {
-        try {
-            const data = JSON.parse(item);
-            if (data.expires_at && data.expires_at < Date.now()) {
-                localStorage.removeItem(variable);
-                return null;
-            }
-            else {
-                return data.value;
-            }
+      try {
+        const data = JSON.parse(item);
+        if (data.expires_at && data.expires_at < Date.now()) {
+          localStorage.removeItem(variable);
+          return null;
+        } else {
+          return data.value;
         }
-        catch (e) {
-            localStorage.removeItem(variable);
-        }
+      } catch (e) {
+        localStorage.removeItem(variable);
+      }
     }
-    return null;
+  }
+  return null;
 }
 export const ls = { set, get, remove };
 export default ls;
--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,38 @@ interface StoredData<T> {
 }
 
 function set<T>(variable: string, value: T, ttl_ms?: number): void {
-  let data: StoredData<T> = {
-    value: value,
-  };
-  if (ttl_ms) {
-    data.expires_at = Date.now() + ttl_ms;
+  if (localStorage) {
+    let data: StoredData<T> = {
+      value: value,
+    };
+    if (ttl_ms) {
+      data.expires_at = Date.now() + ttl_ms;
+    }
+    localStorage.setItem(variable, JSON.stringify(data));
   }
-  localStorage.setItem(variable, JSON.stringify(data));
 }
 
 function remove(variable: string): void {
-  localStorage.removeItem(variable);
+  if (localStorage) {
+    localStorage.removeItem(variable);
+  }
 }
 
 function get<T>(variable: string): T | null {
-  const item = localStorage.getItem(variable);
-  if (item) {
-    try {
-      const data: StoredData<T> = JSON.parse(item) as StoredData<T>;
-      if (data.expires_at && data.expires_at < Date.now()) {
+  if (localStorage) {
+    const item = localStorage.getItem(variable);
+    if (item) {
+      try {
+        const data: StoredData<T> = JSON.parse(item) as StoredData<T>;
+        if (data.expires_at && data.expires_at < Date.now()) {
+          localStorage.removeItem(variable);
+          return null;
+        } else {
+          return data.value;
+        }
+      } catch (e) {
         localStorage.removeItem(variable);
-        return null;
-      } else {
-        return data.value;
       }
-    } catch (e) {
-      localStorage.removeItem(variable);
     }
   }
   return null;
